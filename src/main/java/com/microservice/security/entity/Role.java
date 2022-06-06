dd6f51d86320e9447b9cde2458088e0b402ba53c
ducnguyendinh1302@gmail.com <@Ducn1321996>
2022-06-07 01:23:36 +0700

    authenticate

---
+++ src/main/java/com/microservice/security/entity/Role.java
@@ -0,0 +1,31 @@
+package com.microservice.security.entity;
+// imports
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.security.Permission;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+}
